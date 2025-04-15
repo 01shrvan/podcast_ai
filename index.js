ab247d9cae6ece7568ef2f9515e3a65c9ee6101d
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-04-15 12:10:50 +0530

    feat: update dependencies and add Expo config with settings and docs

--- index.js
+++ index.js
@@ -2,8 +2,24 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+//override text scaling
+if (Text.defaultProps) {
+  Text.defaultProps.allowFontScaling = false;
+} else {
+  Text.defaultProps = {};
+  Text.defaultProps.allowFontScaling = false;
+}
+
+// override text scaling for inpur fields
+if (TextInput.defaultProps) {
+  TextInput.defaultProps.allowFontScaling = false;
+} else {
+  TextInput.defaultProps = {};
+  TextInput.defaultProps.allowFontScaling = false;
+}
 
 AppRegistry.registerComponent(appName, () => App);
